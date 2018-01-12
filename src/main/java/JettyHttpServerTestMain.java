0f9d99c4d7c7e304da03e603e04da0a81b5475a8
William Berg <william.berg@bekk.no>
2018-01-12 09:38:21 +0100

    Depend on snapshot; NPE-fix confirmed

--- src/main/java/JettyHttpServerTestMain.java
+++ src/main/java/JettyHttpServerTestMain.java
@@ -1,12 +1,14 @@
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 
 public class JettyHttpServerTestMain {
 
     public static void main(String[] args){
         try {
-            HttpServer.create(); // throws NPE
+            HttpServer httpServer = HttpServer.create();// does not throw NPE 🎉
+            httpServer.bind(new InetSocketAddress("localhost", 9620), 0);
         } catch (IOException e) {
             e.printStackTrace();
         }
